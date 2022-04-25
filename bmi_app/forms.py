479b57dd93334f412e9c72f238f4e95a8b5ba9c4
c1228j <cen201228.cjk@gmail.com>
2022-04-25 16:39:50 +0900

    ０に対するエラー処理を行いました。

--- bmi_app/forms.py
+++ bmi_app/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+def check_zero(value):
+    if value == 0:
+        raise ValidationError("0以外の数字を入力してください")
 
 class YourDataForm(forms.Form):
     weight = forms.IntegerField(label='体重(kg)')
-    height = forms.IntegerField(label='身長(cm)')
+    height = forms.IntegerField(label='身長(cm)', validators=[check_zero])
